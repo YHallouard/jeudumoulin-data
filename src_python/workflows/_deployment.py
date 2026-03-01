64252bf49e79cf75ea0642fc705eaff753e6a62a
HALLOUARD <57447861+YHallouard@users.noreply.github.com>
2026-03-02 00:55:25 +0100

    feat: Send config (#4)

--- src_python/workflows/_deployment.py
+++ src_python/workflows/_deployment.py
@@ -1,41 +1,30 @@
+from cli.utils import yaml_arg
+
 from workflows._flows import train_alphazero_flow
 
 IMAGE = "ghcr.io/yhallouard/jeudumoulin/jeudumoulin-worker:latest"
+MLFLOW_TRACKING_URI = "http://mlflow.mlops.svc.cluster.local:5000"
 
 if __name__ == "__main__":
+    alphazero_config = yaml_arg("config/train_alphazero_light.yaml")["config"]
+
     train_alphazero_flow.deploy(
         name="train-alphazero-k8s",
         work_pool_name="kubernetes-homelab",
         image=IMAGE,
         parameters={
-            "config_path": "config/train_alphazero_light.yaml",
-            "mlflow_tracking_uri": "http://mlflow.mlops.svc.cluster.local:5000",
+            "raw_config": alphazero_config,
+            "mlflow_tracking_uri": MLFLOW_TRACKING_URI,
         },
     )
 
+    # dqn_config = yaml_arg("config/train_dqn.yaml")["config"]
     # train_dqn_flow.deploy(
     #     name="train-dqn-k8s",
     #     work_pool_name="kubernetes-homelab",
+    #     image=IMAGE,
     #     parameters={
-    #         "config_path": "config/train_dqn.yaml",
-    #         "mlflow_tracking_uri": "http://mlflow.mlops.svc.cluster.local:5000",
-    #     },
-    # )
-
-    # train_alphazero_flow.deploy(
-    #     name="train-alphazero-local",
-    #     work_pool_name="local-dev",
-    #     parameters={
-    #         "config_path": "config/train_alphazero.yaml",
-    #         "mlflow_tracking_uri": "https://mlflow.yannhallouard.com",
-    #     },
-    # )
-
-    # train_dqn_flow.deploy(
-    #     name="train-dqn-local",
-    #     work_pool_name="local-dev",
-    #     parameters={
-    #         "config_path": "config/train_dqn.yaml",
-    #         "mlflow_tracking_uri": "https://mlflow.yannhallouard.com",
+    #         "raw_config": dqn_config,
+    #         "mlflow_tracking_uri": MLFLOW_TRACKING_URI,
     #     },
     # )
